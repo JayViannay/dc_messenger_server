39b14629c625f3ed45d0e35300dfd361b5130565
nayesoum <soumarenaye@hotmail.fr>
2022-07-25 16:10:38 +0200

    update usercontroller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -41,7 +41,14 @@ router
     
     // ::TODO
     .get('/:id/conversations', async (req, res) => {
-        //::todo
+        try{
+            const conversation = await UserModel.getConversations(Number(req.params.id));
+            conversation ? (
+                res.json(conversation).status(200)
+            ) : res.json({ message : 'User not found' }).status(404);
+        } catch (err) {
+            res.json({ message : 'Error', error : err }).status(500);
+        }
     });
 
 export default router;
