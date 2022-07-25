6471c1a521ea99b83d336e4b777dcdece291b8dd
Sabrina <sabrinahmed47@gmail.com>
2022-07-25 16:13:17 +0200

    update UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -41,7 +41,15 @@ router
     
     // ::TODO
     .get('/:id/conversations', async (req, res) => {
-        //::todo
-    });
+            try {
+                const conversations = await UserModel.getConversations(Number(req.params.id));
+                conversations ? (
+                    res.json(conversations).status(200)
+                ) : res.json({ message : 'Not conversation yet', error : err }).status(404);
+            } catch (err) {
+                res.json({ message : 'Error', error : err }).status(500);
+            }
+        });
+    
 
 export default router;
