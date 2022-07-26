19d30030bc4148863df6602d084d89bff1a470b4
bibiw123 <bibiw123@yahoo.fr>
2022-07-26 15:14:50 +0100

    update ConvModel et ConvController

--- src/controllers/ConversationController.js
+++ src/controllers/ConversationController.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
+import ConversationModel from '../models/ConversationModel.js';
+
 
 const router = express.Router();
 
@@ -9,7 +11,17 @@ router
     })
 
     .get('/:id/messages', async (req, res) => {
-        //::todo
+        
+        try {
+            const Conv = await ConversationModel.getMessages(Number(req.params.id));
+            if (Conv) {
+                const results = await ConversationModel.getMessages(Number(req.params.id))
+                res.json(results).status(200);
+            } else res.json({ message: 'User not found' }).status(404);
+        } catch (err) {
+            res.json({ message: 'Error', error: err }).status(500);
+
+        }
     })
 
     .post('/', async (req, res) => {
