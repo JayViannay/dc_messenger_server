cbc94a98b3f492dfd617a400b2e7326ea639f957
Souad <souadslama09@gmail.com>
2022-07-26 16:06:12 +0200

    add getMessages - show message by conv id - add getParticipants - show conversation avec participants get('/:id/participants',..)

--- src/controllers/ConversationController.js
+++ src/controllers/ConversationController.js
@@ -1,15 +1,33 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
+import ConversationModel from '../models/ConversationModel.js';
 
 const router = express.Router();
 
 router
     .get('/:id/participants', async (req, res) => {
-        //::todo
+        try {
+            const participant = await ConversationModel.getParticipants(Number(req.params.id));
+            participant ? (
+                res.json(participant).status(200)
+            ) : res.json({ message : 'User not found' }).status(404);
+        } catch (err) {
+          res.json({ message : 'Error', error : err }).status(500);
+        }
     })
 
     .get('/:id/messages', async (req, res) => {
-        //::todo
+        try {
+            const convExist = await ConversationModel.getMessages(Number(req.params.id));/* req.params.id est converti en integer en mettant number car c'est string a la base*/
+           
+            if (convExist){
+                const results = await ConversationModel.getMessages(Number(req.params.id));
+                res.json(results).status(200)
+             }else res.json({ message : 'conversation not found' }).status(404);
+
+        } catch (err) {
+            res.json({ message : 'Error', error : err }).status(500);
+        }
     })
 
     .post('/', async (req, res) => {
--- src/models/ConversationModel.js
+++ src/models/ConversationModel.js
@@ -1,12 +1,26 @@
 /* eslint-disable no-unused-vars */
 import db from './_index.js';
 
-const getMessages = (id) => {
-    //::todo
+const getMessages = (id) => { /* le id est celui d'une conversation */
+return new Promise((resolve, reject) => {
+    db.query('SELECT * FROM message WHERE conversation_id = ?', id, (err, results) => {/* ma requête me retourne une erreur oubien resultat dans results */
+        
+    if (err) reject(err);
+        else resolve(results);
+        
+    });
+});
 };
 
-const getParticipants = (id) => {
-    //::todo
+const getParticipants = (idUser) => {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT * FROM conversation_user inner Join user WHERE id = conversation_user.user_id', idUser, (err, results) => {/* ma requête me retourne une erreur oubien resultat dans results */
+            
+        if (err) reject(err);
+            else resolve(results);
+            
+        });
+   });
 };
 
 const updateLastMessageId = (id, messageId) => {
