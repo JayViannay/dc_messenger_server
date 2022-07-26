5af4aeb165675707be72fad1b8e5d1ba7d017f77
Albertah-R <albertah.razaf@gmail.com>
2022-07-26 12:18:27 +0200

    fix conflict

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -46,24 +46,21 @@ router
 
 // ::TODO
 /* Une route qui sera utilisée pour récupérer toutes les conversations d'un utilisateur. 
-    get : méthode du routeur express. Il est utilisé pour définir un itinéraire.*/
-    .get('/:id/conversations', async (req, res) => {
-        try {
-<<<<<<< HEAD
-            /* C'est un opérateur ternaire. C'est une déclaration abrégée if/else. 
+    get : méthode du routeur express. Il est utilisé pour définir un itinéraire.
+    /* C'est un opérateur ternaire. C'est une déclaration abrégée if/else. 
             const results = await UserModel.getConversations(Number(req.params.id));
             results ? res.json(results).status(200)
                     : res.json({ message: 'User not found' }).status(404);
         } catch (err) {
             Attraper une erreur et renvoyer un objet json avec un message et l'erreur. */
-=======
+    .get('/:id/conversations', async (req, res) => {
+        try {
             const userExist = await UserModel.find(Number(req.params.id));
             if (userExist) {
                 const results = await UserModel.getConversations(Number(req.params.id))
                 res.json(results).status(200);
             } else res.json({ message: 'User not found' }).status(404);
         } catch (err) {
->>>>>>> d30d41b27a1905f153d572055454471970d73cb9
             res.json({ message: 'Error', error: err }).status(500);
         }
     });
