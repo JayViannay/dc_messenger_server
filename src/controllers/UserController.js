161e6a0f82906343b3c6822d37780169744d954b
JennyViannay <jenny.viannay75@gmail.com>
2022-07-26 11:41:13 +0200

    add feature list conversation by user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import UserModel from '../models/UserModel.js';
 
-const router = express.Router(); 
+const router = express.Router();
 
 router
     .get('/', async (req, res) => {
         try {
             res.json(await UserModel.findAll()).status(200);
         } catch (err) {
-            res.json({ message : 'Error', error : err }).status(500);
+            res.json({ message: 'Error', error: err }).status(500);
         }
     })
 
@@ -17,9 +17,9 @@ router
             const result = await UserModel.find(Number(req.params.id));
             result ? (
                 res.json(result).status(200)
-            ) : res.json({ message : 'User not found' }).status(404);
+            ) : res.json({ message: 'User not found' }).status(404);
         } catch (err) {
-            res.json({ message : 'Error', error : err }).status(500);
+            res.json({ message: 'Error', error: err }).status(500);
         }
     })
 
@@ -31,17 +31,26 @@ router
                 if (user) res.json({ errors: 'User already exist !' }).status(409);
                 else {
                     const userId = await UserModel.add(newUser);
-                    res.json({ success: 'User added successfully !', new_user_id : userId }).status(200);
+                    res.json({ success: 'User added successfully !', new_user_id: userId }).status(200);
                 }
             } catch (err) {
                 res.json({ errors: err }).status(500);
             }
-        } else res.json({ errors : 'All fields are required : email & password' }).status(409);
+        } else res.json({ errors: 'All fields are required : email & password' }).status(409);
     })
-    
-    // ::TODO
+
     .get('/:id/conversations', async (req, res) => {
-        //::todo
+
+        try {
+            const userExist = await UserModel.find(Number(req.params.id));
+            if (userExist) {
+                const results = await UserModel.getConversations(Number(req.params.id))
+                res.json(results).status(200);
+            } else res.json({ message: 'User not found' }).status(404);
+        } catch (err) {
+            res.json({ message: 'Error', error: err }).status(500);
+        }
+        
     });
 
 export default router;
