fcecf7ce446307c48bb7757d182932a4cc96742d
Albertah-R <albertah.razaf@gmail.com>
2022-07-26 23:57:06 +0200

    update conversationModel and MessageModel

--- src/models/ConversationModel.js
+++ src/models/ConversationModel.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import db from './_index.js';
 
-/* Une fonction qui prend un identifiant et renvoie les messages. */
+/* Une fonction qui prend un identifiant et renvoie les messages. SELECT * FROM `message` WHERE `conversation_id` = ?;*/
 const getMessages = (id) => {
     return new Promise((resolve, reject) => {
         db.query('SELECT * FROM message WHERE conversation_id = ?', id, (err, results) => {
@@ -14,6 +14,7 @@ const getMessages = (id) => {
 /* Une fonction qui prend un identifiant et renvoie les participants. */
 const getParticipants = (id) => {
     return new Promise((resolve, reject) => {
+        /* Sélection de tous les utilisateurs qui sont dans la conversation avec l'identifiant passé en paramètre. */
         db.query('SELECT * FROM user WHERE id IN (SELECT user_id FROM conversation_user WHERE conversation_id = ?)', id, (err, results) => {
             if (err) reject(err);
             else resolve(results);
@@ -24,6 +25,7 @@ const getParticipants = (id) => {
 /* Une fonction qui prend deux paramètres, id et messageId, et renvoie une valeur. */
 const updateLastMessageId = (id, messageId) => {
     return new Promise((resolve, reject) => {
+        /* Mise à jour du dernier identifiant de message d'une conversation. UPDATE `conversation` SET `last_message_id` = '?' WHERE `conversation`.`id` = ?;*/
         db.query('UPDATE conversation SET last_message_id = ? WHERE id = ?',
             [messageId, id],
             (err, result) => {
@@ -34,13 +36,14 @@ const updateLastMessageId = (id, messageId) => {
 
 };
 
-/* Une fonction qui ajoute une conversation à la base de données. */
+/* Une fonction qui ajoute une conversation à la base de données. INSERT INTO `conversation` (`id`, `last_message_id`) VALUES (NULL, NULL);*/
 const add = () => {
 };
 
-/* Une fonction qui prend deux paramètres, conversation_id et user_id, et renvoie une valeur. */
+/* Une fonction qui prend deux paramètres, conversation_id et user_id, et renvoie une valeur.*/
 const addParticipant = (conversation_id, user_id) => {
     return new Promise((resolve, reject) => {
+        /* Insertion d'un nouveau conversation_user dans la base de données. */
         db.query('INSERT INTO conversation_user (conversation_id, user_id) VALUES (?, ?)',
             [conversation_id, user_id],
             (err, result) => {
--- src/models/MessageModel.js
+++ src/models/MessageModel.js
@@ -4,8 +4,8 @@ import db from './_index.js';
 /* Une fonction qui prend un message comme paramètre. */
 const add = (message) => {
     return new Promise((resolve, reject) => {
-        db.query('INSERT INTO message (conversation_id, author_id, content) VALUES (?, ?, ?)',
-            [message.conversation_id, message.author_id, message.content],
+        db.query('INSERT INTO message (author_id, conversation_id, created_at, content) VALUES (?, ?, ?, ?)',
+            [message.author_id, message.conversation_id, message.created_at, message.content],
             (err, result) => {
                 if (err) reject(err);
                 else resolve(result.insertId);
