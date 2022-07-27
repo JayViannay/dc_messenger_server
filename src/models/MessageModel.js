6ce54be525f1b0691e0075eda026067ffc6bd8dd
unknown <fatihabenou2021@gmail.com>
2022-07-27 12:18:10 +0200

    je suis larguée donc je vais puller

--- src/models/MessageModel.js
+++ src/models/MessageModel.js
@@ -7,6 +7,16 @@ import db from './_index.js';
  */
 const add = (message) => {
     //::todo
+    const add = (message) => {
+        const { author_id, conversation_id, created_at, content } = message;
+        return new Promise((resolve, reject) => {
+            db.query('INSERT INTO message (author_id, conversation_id, created_at, content) VALUES (?, ?)',
+                [email, password],
+                (err, result) => {
+                    if (err) reject(err);
+                    else resolve(result.insertId);
+                });
+        });
 };
 
 export default { add };
