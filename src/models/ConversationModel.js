0718049536a64f9b14fdfea48dbbd148752c3b9f
unknown <fatihabenou2021@gmail.com>
2022-07-27 11:27:01 +0200

    bof

--- src/models/ConversationModel.js
+++ src/models/ConversationModel.js
@@ -25,7 +25,12 @@ const getMessages = (id) => {
  * 
  */
 const getParticipants = (id) => {
-    //::todo
+    return new Promise((resolve, reject) => {
+        db.query('SELECT user.id user.email FROM conversation_user INNER JOIN user ON conversation_user.user_id = user.id WHERE conversation_id = 1', (err, results) => {
+            if (err) reject(err);
+            else resolve(results);
+        });
+    });
 };
 
 /**
